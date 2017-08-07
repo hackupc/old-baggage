dd77867e43f0e45ccdabb67983db62ab4e93d632
Oriol Closa Márquez <oriol20@gmail.com>
2017-08-07 13:34:43 +0200

    Started again.

--- assets/functions/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_unset();
-    session_destroy();
-    if(isset($_COOKIE['username'])) setcookie('username',$_COOKIE['username'],time()-30);
-    header("Location: ../../");
-?>
