d3d2805df0640cba8b2945000ffcb9a5e73abe70
oriol20 <oriol20@gmail.com>
2017-08-08 20:19:19 +0200

    Added header.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+<div id="main-wrap">
+  <div id="user-wrap">
+    Test.
+  </div>
+  <div id="pos-wrap">
+    Test 2.
+  </div>
 </div>
 @endsection
